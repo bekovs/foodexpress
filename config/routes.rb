0dbc9dcfc44f03f4858c9605773c8e078cee8493
Unknown <bekovs96@gmail.com>
2017-10-09 12:26:59 +0600

    active admin installed and added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  ActiveAdmin.routes(self)
+	root 'pages#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
